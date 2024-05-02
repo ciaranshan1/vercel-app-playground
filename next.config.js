d25b9e0dbf40b20af814671aed182b32bdacb69f
ciaranshan1 <ciaran.shanahan@wpengine.com>
2024-05-02 15:43:00 +0100

    use atlas cache handler

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const { withAtlasConfig } = require('@wpengine/atlas-next');
+
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = withAtlasConfig(nextConfig, {});
